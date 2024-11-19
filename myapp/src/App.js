c4aeab8edf78e4bed6827b5bc148047a2561503a
Nipun Dilshan <139701467+mrnipundilshan@users.noreply.github.com>
2024-11-19 09:33:44 +0000

    new changes

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Childcom from './Childcom';
 
 function App() {
+  const[childname,setChildname] = useState('Default');
+  const receivename = (name)=>{
+    setChildname(name);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Hello React I'm Parent</h2>
+      <Childcom setfun={receivename}/>
+      <p>Parent:My child said:{childname}</p>
     </div>
   );
 }
